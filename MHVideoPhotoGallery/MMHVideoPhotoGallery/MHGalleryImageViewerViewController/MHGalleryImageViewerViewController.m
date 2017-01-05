d6e0d528b89bdff4f5ff3a7810bd56cd66b57adf
Carlos Crane <ccrama8@gmail.com>
2017-01-05 15:22:44 -0600

    Update MHGalleryImageViewerViewController.m

--- MHVideoPhotoGallery/MMHVideoPhotoGallery/MHGalleryImageViewerViewController/MHGalleryImageViewerViewController.m
+++ MHVideoPhotoGallery/MMHVideoPhotoGallery/MHGalleryImageViewerViewController/MHGalleryImageViewerViewController.m
@@ -56,7 +56,7 @@
     [super viewDidAppear:animated];
     
     self.navigationController.delegate = self;
-    self.navigationController.navigationBar.barTintColor = UIColor.black
+    self.navigationController.navigationBar.barTintColor = UIColor.blackColor
 }
 
 -(void)viewWillAppear:(BOOL)animated{
